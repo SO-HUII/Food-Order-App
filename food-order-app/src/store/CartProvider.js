92518c2a22974c250c7462bc000145bd8e65f3c3
SO HUI <haejuuun925@gmail.com>
2023-10-27 21:20:14 +0900

    feat: 장바구니 Reducer 추가

--- food-order-app/src/store/CartProvider.js
+++ food-order-app/src/store/CartProvider.js
@@ -1,17 +1,38 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import CartContext from './cart-context';
+
+const defaultCartState = {
+    items: [],
+    totalAmount: 0
+};
+
+const cartReducer = (state, action) =>{
+    if(action.type === 'ADD') {
+        const updatedItems = state.items.concat(action.item);
+        const newTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+        return {
+            items: updatedItems,
+            totalAmount: newTotalAmount
+        }
+    }
+    return defaultCartState;
+};
 
 // CartContext의 데이터 관리 및 그 컨텍스트에 접근하려는 모든 컴포넌트에 제공하는 것을 위한 컴포넌트
 const CartProvider = (props) => {
-    const addItemToCartHandler = item => {};
+    const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState);
+
+    const addItemToCartHandler = item => {
+        dispatchCartAction({type: 'ADD', item: item});
+    };
 
     const removeItemFromCartHandler = id => {
-
+        dispatchCartAction({type: 'REMOVE', id: id});
     };
 
     const cartContext = {
-        items: [],
-        totalAmount: 0,
+        items: cartState.items,
+        totalAmount: cartState.totalAmount,
         addItem: addItemToCartHandler,
         removeItem: removeItemFromCartHandler
     };
