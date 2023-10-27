0b1b8bef9e5e92d87513f836a6b2c6dfe8f97df0
SO HUI <haejuuun925@gmail.com>
2023-10-27 21:21:27 +0900

    feat: item 장바구니 추가, ref사용

--- food-order-app/src/components/Meals/MealItem/MealItem.js
+++ food-order-app/src/components/Meals/MealItem/MealItem.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import classes from './MealItem.module.css';
 import MealItemForm from './MealItemForm';
+import CartContext from '../../../store/cart-context';
 
 const MealItem = (props) => {
+    const cartCtx = useContext(CartContext);
+
     // 가격 소수점 두 번째 자리까지만 나타남.
     const price = `$${props.price.toFixed(2)}`;
+
+    const addToCartHandler = amount => {
+        cartCtx.addItem({
+            id: props.id,
+            name: props.name,
+            amount: amount,
+            price: props.price
+        });
+    };
+
     return (
         <li className={classes.meal}>
             <div>
@@ -13,7 +26,7 @@ const MealItem = (props) => {
                 <div className={classes.price}>{price}</div>
             </div>
             <div>
-                <MealItemForm id={props.id} />
+                <MealItemForm onAddToCart={addToCartHandler} />
             </div>
         </li>
     );
