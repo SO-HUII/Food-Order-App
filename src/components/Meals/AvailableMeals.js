2f5e7ccf8339d3720cce85d3fe4a2348ab481d3c
SO HUI <haejuuun925@gmail.com>
2023-12-04 15:27:22 +0900

    feat: 오류 처리

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -6,10 +6,16 @@ import MealItem from './MealItem/MealItem';
 const AvailableMeals = () => {
     const [meals, setMeals] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
+    const [httpError, setHttpError] = useState();
 
     useEffect(() => {
         const fetchMeals = async () => {
-            const response = await fetch('https://food-order-13464-default-rtdb.firebaseio.com/meals').then();
+            const response = await fetch('https://food-order-13464-default-rtdb.firebaseio.com/meals.json').then();
+            
+            if(!response.ok) {
+                throw new Error('Something went wrong!');
+            }
+
             const responseData = await response.json();
 
             const loadMeals = [];
@@ -25,13 +31,25 @@ const AvailableMeals = () => {
             setMeals(loadMeals);
             setIsLoading(false);
         };
-        fetchMeals();
+
+        fetchMeals().catch((error) => {
+            setIsLoading(false);
+            setHttpError(error.message);
+        });
     }, []);
 
     if(isLoading) {
         return (
             <section className={classes.MealsLoading}>
                 <p>Loading...</p>
+            </section>
+        );
+    }
+
+    if(httpError) {
+        return( 
+            <section className={classes.MealsError}>
+                <p>{httpError}</p>
             </section>
         );
     }
